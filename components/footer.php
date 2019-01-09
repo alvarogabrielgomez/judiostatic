a9dfd37d4b48e875a3379a2696f611d4259ce1cc
Alvaro Gabriel Gomez <alvarogabrielgomez@gmail.com>
2019-01-09 20:43:41 -0200

    Ene09aaaa

--- components/footer.php
+++ components/footer.php
@@ -1,27 +1,55 @@
 <footer class="footer">
 <div id="ir-arriba"><a href="#">Ir Arriba</a></div>
+
+<div class="f-container">
     <div class="b1">
+    <div class="navFooterHeader">Conozca sobre nosotros</div>
+    <ul class="navFooter">
+        <li class="">
+            <a href="" class="">Trabajos</a>
+        </li>
+        <li>
+            <a href="" class="">Blog</a>
+        </li>
+        <li>
+            <a href="" class="">Sobre Judiostatic</a>
+        </li>
 
-        <img src="" alt="">
-
+    </ul>
     </div>
 
 
 	<div class="b2">
 
-        <div id="footer-container">
-           
-            <p class="enlaces">ENLACES</p>
-            <nav class="enlaces">
-                <ul>
-	                <li><a onClick="cargarContenido('inicio.html')"  href="#main">inicio</a></li>
-			        <li><a onClick="cargarContenido('about.html')"  href="#main">about</a></li>
-			        <li><a onClick="cargarContenido('contacto.html')"  href="#main">contacto</a></li>
-                </ul>
-            </nav>
+    <div class="navFooterHeader">Venda con nosotros</div>
+    <ul class="navFooter">
+        <li class="">
+            <a href="" class="">Venda sus cupones en Judiostatic</a>
+        </li>
+        <li>
+            <a href="" class="">Como comenzar con nosotros</a>
+        </li>
 
-        </div>
+
+    </ul>
     </div>
+
+    <div class="b3">
+    <div class="navFooterHeader">Deja que te ayudemos</div>
+    <ul class="navFooter">
+        <li class="">
+            <a href="" class="">Tu cuenta</a>
+        </li>
+        <li>
+            <a href="" class="">Informar algun error</a>
+        </li>
+        <li>
+            <a href="" class="">Aportar alguna sugerencia</a>
+        </li>
+
+    </ul>    </div>
+    </div>
+    <div id="creditos"><div class="logo-creditos">JUDIOSTATIC</div></div>
 
     <div id="creditos"><div><a href="#">Condiciones de Uso</a> <a href="#">Privacidad</a><a href="http://ckj1.site11.com"> © Alvaro Gabriel Gomez</a>. TODOS LOS DERECHOS RESERVADOS</div></div>
 
